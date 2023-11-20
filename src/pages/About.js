a31280ebf233b44f38c3daf1c088e3049eed4b82
KaungMN <kaungmyatnoe2016@gmail.com>
2023-11-20 23:23:00 +0630

    React Router-1

---
+++ src/pages/About.js
@@ -0,0 +1,21 @@
+export default function About() {
+  return (
+    <div className="about">
+      <h2>About Us</h2>
+      <p>
+        Welcome to KMN! We're delighted to have you here. Our blog is a passion
+        project dedicated to demystifying the intricacies of React and guiding
+        you through the exciting world of web development.
+      </p>
+      <h3>Our Mission</h3>
+      <p>
+        Our mission is to make learning React and mastering routing accessible
+        to everyone, from beginners to seasoned developers. We believe in the
+        power of well-crafted content to transform complex concepts into clear,
+        actionable insights. Whether you're taking your first steps into React
+        or seeking to deepen your understanding of routing, we've got you
+        covered.
+      </p>
+    </div>
+  );
+}
